160de3ecb927b39fac18607f90fe001750b5fe35
OBeaude <62885463+OBeaude@users.noreply.github.com>
2024-11-08 14:36:01 +0000

    Fix pb with ERAA data reading for UC tutorial

--- tutorial_long_term_uc/data_reader.py
+++ tutorial_long_term_uc/data_reader.py
@@ -3,19 +3,19 @@ from typing import Dict, List
 import pandas as pd
 import numpy as np
 
+from common.constants_datatypes import DATATYPE_NAMES
 from long_term_uc_io import INPUT_ERAA_FOLDER, DT_SUBFOLDERS, DT_FILE_PREFIX, COLUMN_NAMES, \
-    FILES_FORMAT, RES_PROD_FILE_PREFIX
-from utils.basic_utils import get_key_of_val
+    FILES_FORMAT
 from utils.df_utils import concatenate_dfs, selec_in_df_based_on_list, set_aggreg_col_based_on_corresp
 
 
-def set_aggreg_cf_prod_types_data(df_cf_list: List[pd.DataFrame], pt_agg_col: str, val_col: str,
+def set_aggreg_cf_prod_types_data(df_cf_list: List[pd.DataFrame], prod_type_col: str, pt_agg_col: str, val_col: str,
                                   agg_prod_types_def: Dict[str, List[str]]) -> pd.DataFrame:
     # concatenate, aggreg. over prod type of same aggreg. type and avg
     df_cf_agg = concatenate_dfs(dfs=df_cf_list)
-    df_cf_agg = set_aggreg_col_based_on_corresp(df=df_cf_agg, agg_col_name=pt_agg_col, 
+    df_cf_agg = set_aggreg_col_based_on_corresp(df=df_cf_agg, col_name=prod_type_col, agg_col_name=pt_agg_col, 
                                                 val_col=val_col, agg_corresp=agg_prod_types_def,
-                                                aggreg_ope=np.mean)
+                                                aggreg_ope="mean")
     return df_cf_agg
 
 
@@ -40,15 +40,18 @@ def get_countries_data(countries: List[str], year: int, climatic_year: int,
     decimal_sep = FILES_FORMAT.decimal_sep
 
     demand = {}
-    cf_data = {}
     agg_cf_data = {}
 
+    n_spaces_msg = 2
+
     for country in countries:
+        print(f"For country: {country}")
         # read csv files
         # [Coding trick] f"{year}_{country}" directly fullfill string with value of year 
         # and country variables (f-string completion)
         current_suffix = f"{year}_{country}"  # common suffix to all ERAA data files
         # get demand
+        print("Get demand")
         current_df_demand = pd.read_csv(f"{demand_folder}/{demand_prefix}_{current_suffix}.csv", sep=";", index_col=1, parse_dates=True)
         # then keep only selected climatic year
         demand[country] = selec_in_df_based_on_list(df=current_df_demand, 
@@ -56,33 +59,38 @@ def get_countries_data(countries: List[str], year: int, climatic_year: int,
                                                     selec_vals=[climatic_year], rm_selec_col=True)
 
         # get RES capacity factor data
-        cf_data[country] = {}
+        print("Get RES capacity factors")
+        agg_cf_data[country] = {}
         for agg_prod_type in selec_prod_types[country]:
             # if prod type with CF data
             if agg_prod_type in agg_prod_types_with_cf_data:
+                print(n_spaces_msg * " " + f"- For aggreg. prod. type: {agg_prod_type}")
                 current_df_res_cf_list = []
-                for prod_type in aggreg_prod_types_def[agg_prod_type]:
-                    prod_type_prefix = RES_PROD_FILE_PREFIX[prod_type]
-                    cf_data_file = f"{res_cf_folder}/{res_cf_prefix}_{prod_type_prefix}_{current_suffix}.csv"
+                aggreg_pt_cf_def = aggreg_prod_types_def[DATATYPE_NAMES.capa_factor]
+                for prod_type in aggreg_pt_cf_def[agg_prod_type]:
+                    cf_data_file = f"{res_cf_folder}/{res_cf_prefix}_{prod_type}_{current_suffix}.csv"
                     if os.path.exists(cf_data_file) is False:
-                        print(f"[WARNING] RES capa. factor data file does not exist: {prod_type} not accounted for here")
+                        print(2*n_spaces_msg * " " + f"[WARNING] RES capa. factor data file does not exist: {prod_type} not accounted for here")
                     else:
+                        print(2*n_spaces_msg * " " + f"* Prod. type: {prod_type}")
                         current_df_res_cf = pd.read_csv(cf_data_file, parse_dates=True, 
                                                         sep=column_sep, decimal=decimal_sep) 
                         current_df_res_cf = \
                             selec_in_df_based_on_list(df=current_df_res_cf, selec_col=climatic_year_col,
                                                       selec_vals=[climatic_year], rm_selec_col=True)
                         if len(current_df_res_cf) == 0:
-                            print(f"[WARNING] No RES capa. factor data for prod. type {prod_type} and climatic year {climatic_year}")
+                            print(2*n_spaces_msg * " " + f"[WARNING] No RES capa. factor data for prod. type {prod_type} and climatic year {climatic_year}")
                         else:
-                            current_df_res_cf_list.append(cf_data[country][prod_type])
+                            # add column with production type (for later aggreg.)
+                            current_df_res_cf[prod_type_col] = prod_type
+                            current_df_res_cf_list.append(current_df_res_cf)
                 if len(current_df_res_cf_list) == 0:
-                    print(f"[WARNING] No data available for aggregate RES prod. type {agg_prod_type} -> not accounted for in UC model here")
+                    print(n_spaces_msg * " " + f"[WARNING] No data available for aggregate RES prod. type {agg_prod_type} -> not accounted for in UC model here")
                 else:
                     # concatenate, aggreg. over prod type of same aggreg. type and avg
                     agg_cf_data[country] = \
-                        set_aggreg_cf_prod_types_data(df_cf_list=current_df_res_cf_list,
+                        set_aggreg_cf_prod_types_data(df_cf_list=current_df_res_cf_list, prod_type_col=prod_type_col,
                                                       pt_agg_col=prod_type_agg_col, val_col=value_col,
-                                                      agg_prod_types_def=aggreg_prod_types_def)
+                                                      agg_prod_types_def=aggreg_pt_cf_def)
     return demand, agg_cf_data
     
